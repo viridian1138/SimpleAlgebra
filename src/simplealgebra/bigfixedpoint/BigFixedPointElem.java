091f7479ff02e48114c5eb5f8cc13f34f07ccb76
viridian1138 <viridian1138@earthlink.net>
2014-03-02 23:30:53 -0700

    Added fixed-point type.

---
+++ src/simplealgebra/bigfixedpoint/BigFixedPointElem.java
@@ -0,0 +1,91 @@
+
+
+
+
+
+//$$strtCprt
+/**
+* Simple Algebra 
+* 
+* Copyright (C) 2014 Thornton Green
+* 
+* This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
+* published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+* This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+* of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+* You should have received a copy of the GNU General Public License along with this program; if not, 
+* see <http://www.gnu.org/licenses>.
+* Additional permission under GNU GPL version 3 section 7
+*
+*/
+//$$endCprt
+
+
+
+
+
+
+package simplealgebra.bigfixedpoint;
+
+import java.math.BigInteger;
+
+import simplealgebra.Elem;
+import simplealgebra.NotInvertibleException;
+
+public class BigFixedPointElem<T extends Precision> extends Elem<BigFixedPointElem<T>, BigFixedPointElemFactory<T>> {
+	
+	public BigFixedPointElem( BigInteger _val , T _prec )
+	{
+		val = _val;
+		prec = _prec;
+	}
+	
+	public BigFixedPointElem( double vl , T _prec ) // Note: Careful!!! Conversion only works with small values.
+	{
+		final BigInteger _val = BigInteger.valueOf( (long)( vl * prec.getVal().doubleValue() ) );
+		val = _val;
+		prec = _prec;
+	}
+
+	@Override
+	public BigFixedPointElem<T> add(BigFixedPointElem<T> b) {
+		return( new BigFixedPointElem<T>( val.add( b.val ) , prec ) );
+	}
+
+	@Override
+	public BigFixedPointElem<T> mult(BigFixedPointElem<T> b) {
+		return( new BigFixedPointElem<T>( val.multiply( b.val ).divide( prec.getVal() ) , prec ) );
+	}
+
+	@Override
+	public BigFixedPointElem<T> negate() {
+		return( new BigFixedPointElem<T>( val.negate() , prec ) );
+	}
+
+	@Override
+	public BigFixedPointElem<T> invert() throws NotInvertibleException {
+		return( new BigFixedPointElem<T>( prec.getValSquared().divide( val ) , prec ) );
+	}
+
+	@Override
+	public BigFixedPointElem<T> divideBy(int vali) {
+		return( new BigFixedPointElem<T>( val.divide( BigInteger.valueOf( vali ) ) , prec ) );
+	}
+
+	@Override
+	public BigFixedPointElemFactory<T> getFac() {
+		return( new BigFixedPointElemFactory<T>( prec ) );
+	}
+	
+	public double toDouble( )
+	{
+		return( ( val.doubleValue() ) / ( prec.getVal().doubleValue() ) );
+	}
+
+	
+	BigInteger val;
+	T prec;
+	
+
+}
+
